f0d419b660aabd857f8be1713b117a121a398f0e
oscarricardosan <oscarricardosan@gmail.com>
2018-02-02 10:30:45 -0500

    El titulo del archivo venga de una función no este quemado

--- src/Report_manController.php
+++ src/Report_manController.php
@@ -14,22 +14,27 @@ class Report_manController extends Controller
      */
     protected $report_man;
 
+    protected function get_file_title()
+    {
+        return "Reporte";
+    }
+
     public function generate_xls(Request $request)
     {
         $this->validate($request, ['fields'=> 'array|required', 'filters'=> 'array|sometimes|required']);
-        return $this->generate($request, 'Reporte de casos', 'xlsx');
+        return $this->generate($request, $this->get_file_title(), 'xlsx');
     }
 
     public function generate_xlsx(Request $request)
     {
         $this->validate($request, ['fields'=> 'array|required', 'filters'=> 'array|sometimes|required']);
-        return $this->generate($request, 'Reporte de casos', 'xls');
+        return $this->generate($request, $this->get_file_title(), 'xls');
     }
 
     public function generate_csv(Request $request)
     {
         $this->validate($request, ['fields'=> 'array|required', 'filters'=> 'array|sometimes|required']);
-        return $this->generate($request, 'Reporte de casos', 'csv');
+        return $this->generate($request, $this->get_file_title(), 'csv');
     }
 
     protected function generate(Request $request, $file_name, $file_extension)
